2fb12850f1a17cff0f6f2efb8c52e40f5e6fbf03
christopherabouabdo <chris@foodbeast.com>
2016-01-19 18:25:53 -0800

    Pass current page index to renderPage function

--- ViewPager.js
+++ ViewPager.js
@@ -236,7 +236,8 @@ var ViewPager = React.createClass({
         render={this.props.renderPage.bind(
           null,
           dataSource.getPageData(pageIdx),
-          pageID
+          pageID,
+          this.state.currentPage
         )}
       />
     );
