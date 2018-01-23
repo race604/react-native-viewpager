1c42884cb5fae0053ee91ec5e742ba57b89ee0c1
Anurag Dadheech <anurag.dadheech@myntra.com>
2018-01-24 01:29:19 +0530

    Viewproptypes

--- ViewPager.js
+++ ViewPager.js
@@ -12,6 +12,7 @@ var {
   PanResponder,
   Animated,
   StyleSheet,
+  ViewPropTypes,
 } = ReactNative;
 
 var StaticRenderer = require('react-native/Libraries/Components/StaticRenderer');
@@ -29,7 +30,7 @@ var ViewPager = createReactClass({
   },
 
   propTypes: {
-    ...View.propTypes,
+    ...ViewPropTypes,
     dataSource: PropTypes.instanceOf(ViewPagerDataSource).isRequired,
     renderPage: PropTypes.func.isRequired,
     onChangePage: PropTypes.func,
